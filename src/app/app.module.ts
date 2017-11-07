7c45a044005285785171d512946dec20000098ff
Francesca Giordano <francesca020406@hotmail.com>
2017-11-07 18:21:44 -0500

    learning pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
+import { CustomPipesComponent } from './custom-pipes/custom-pipes.component';
+import { FormsModule } from '@angular/forms';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuiltInPipesComponent,
+    CustomPipesComponent,
+    CapitalizePipe,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/built-in-pipes/built-in-pipes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-built-in-pipes',
+  templateUrl: './built-in-pipes.component.html',
+  styleUrls: ['./built-in-pipes.component.css']
+})
+export class BuiltInPipesComponent implements OnInit {
+
+ today = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  increment() {                        
+    const changedDate = new Date(this.today);
+
+    // increase the day of the month
+    changedDate.setDate(this.today.getDate() + 1);
+
+    // update our date
+    this.today = changedDate;
+  }
+
+}
